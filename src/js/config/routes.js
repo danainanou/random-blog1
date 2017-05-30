2a2c73457dc1e1710c657ca8664a7f3fbf4d090b
eadpearce <e.pearce@tutanota.com>
2017-05-30 15:26:48 +0100

    Started angular setup

---
+++ src/js/config/routes.js
@@ -0,0 +1,45 @@
+angular
+  .module('duckFacts')
+  .config(Router);
+
+Router.$inject = [
+  '$stateProvider',
+  '$urlRouterProvider',
+  '$locationProvider'
+];
+function Router($stateProvider, $urlRouterProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: '/js/views/home.html',
+      controller: 'MainCtrl',
+      controllerAs: 'main'
+    })
+    .state('postsIndex', {
+      url: '/posts',
+      templateUrl: '/js/views/posts/index.html',
+      controller: 'PostsIndexCtrl',
+      controllerAs: 'posts'
+    })
+    .state('postsShow', {
+      url: '/posts/:id',
+      templateUrl: '/js/views/posts/show.html',
+      controller: 'PostsShowCtrl',
+      controllerAs: 'posts'
+    })
+    .state('postsEdit', {
+      url: '/posts/:id/edit',
+      templateUrl: '/js/views/posts/edit.html',
+      controller: 'PostsEditCtrl',
+      controllerAs: 'posts'
+    })
+    .state('postsNew', {
+      url: '/posts/new',
+      templateUrl: '/js/views/posts/new.html',
+      controller: 'PostsNewCtrl',
+      controllerAs: 'posts'
+    });
+
+  $urlRouterProvider.otherwise('/');
+}
---
+++ src/js/controllers/main.controller.js
@@ -0,0 +1,13 @@
+angular
+  .module('duckFacts')
+  .controller('MainCtrl', MainCtrl);
+
+MainCtrl.$inject = ['$http'];
+function MainCtrl($http) {
+  const vm = this;
+  $http
+    .get('http://localhost:3000/api/users')
+    .then(response => {
+      vm.all = response.data;
+    });
+}
